523ad12384ae62b504886a94b2df1eb4a93b03fc
Anastasiya <anastasiya.ibr@mail.ru>
2024-12-10 13:24:51 +0300

    Страницы сделаны приватными

---
+++ src/Components/Header.jsx
@@ -0,0 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthProvader";
+import { Button } from "./Common";
+import { Logo } from "./Logo";
+import { Navbar } from "./Navbar";
+import { SortingField } from "./SortingField";
+
+export const Header = ({ path }) => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  const handleSignout = () => {
+    auth.signout();
+  };
+
+  const handleSignin = () => {
+    navigate("/login");
+  };
+
+  const watchSortingField = (path) => {
+    if (
+      path === "/characters" ||
+      path === "/locations" ||
+      path === "/episodes"
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  return (
+    <header className="bg-blue-950 flex justify-between items-center px-4">
+      <Logo /> <Navbar />
+      {watchSortingField(path) && <SortingField />}
+      <Button
+        text={auth.user ? "logout" : "login"}
+        width="100px"
+        onClick={auth.user ? handleSignout : handleSignin}
+      />
+    </header>
+  );
+};
