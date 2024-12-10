46d342289c2ed875ec575a0992b0b24ed291604e
Anastasiya <anastasiya.ibr@mail.ru>
2024-12-10 11:34:40 +0300

    feat: созданы компоненты Input и Button

--- src/Components/Common/Button.jsx
+++ src/Components/Common/Button.jsx
@@ -1,6 +1,11 @@
-export const Button = ({ type, onClick, text, className }) => {
+export const Button = ({ type, onClick, text, className, width }) => {
   return (
-    <button type={type} onClick={onClick} className={className}>
+    <button
+      type={type}
+      onClick={onClick}
+      className="text-white bg-[rgb(66,63,235)] px-4 py-2 rounded transition-transform transform hover:scale-105"
+      style={{ width: width ? width : "100%" }}
+    >
       {text}
     </button>
   );
