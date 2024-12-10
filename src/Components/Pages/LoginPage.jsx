b56ee3b9d5f5dd5d4ab4f13ba1f076615b0a5717
Anastasiya <anastasiya.ibr@mail.ru>
2024-12-10 11:40:49 +0300

    style: доб.марджин

--- src/Components/Pages/LoginPage.jsx
+++ src/Components/Pages/LoginPage.jsx
@@ -2,7 +2,7 @@ import { Button, Input } from "../Common";
 
 export const LoginPage = () => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center my-40">
       <form className="w-full max-w-sm">
         <Input />
         <Button text="login" />
