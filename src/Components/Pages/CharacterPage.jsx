50d85d90ec12bde6f566867e066904262970e332
Anastasiya <anastasiya.ibr@mail.ru>
2024-12-13 13:04:36 +0300

    feat:добавлено получение данных для страницы элемента

--- src/Components/Pages/CharacterPage.jsx
+++ src/Components/Pages/CharacterPage.jsx
@@ -1,41 +1,35 @@
 import { useParams } from "react-router-dom";
-import { useFetchArray } from "../../hooks/useFetchArray";
-import { findElem } from "../../utils/findElem";
-import { PagesLayout } from "./PagesLayout";
-import { useEffect, useState } from "react";
+import { useFetchOneElem } from "../../hooks/useFetchOneElem";
 
 export const CharacterPage = () => {
-  const [currentItem, setCurrentItem] = useState(null);
-  const { arr, isLoading, error } = useFetchArray("characters");
   const params = useParams();
-
-  useEffect(() => {
-    if (arr.length > 0) {
-      setCurrentItem(findElem(arr, params.id));
-    }
-  }, [arr, params.id]);
+  const { elem, error, isLoading } = useFetchOneElem("character", params.id);
 
   return (
-    <PagesLayout isLoading={isLoading} error={error}>
-      {currentItem && (
+    <div>
+      {isLoading ? (
+        <div>loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
         <div className="p-4 flex justify-between items-center w-[700px] m-auto my-[100px]">
           <img
             className="w-30 h-30 object-cover rounded-lg mb-4"
-            src={currentItem.image}
-            alt={currentItem.name}
+            src={elem.image}
+            alt={elem.name}
           />
 
           <div className="text-lg font-bold text-[rgb(80,169,197)] ">
-            <div>Id: {currentItem.id}</div>
-            <div>Created: {currentItem.created}</div>
-            <div>Name: {currentItem.name}</div>
-            <div>Status: {currentItem.status}</div>
-            <div>Type: {currentItem.type}</div>
-            <div>Gender: {currentItem.gender}</div>
-            <div>Species: {currentItem.species}</div>
+            <div>Id: {elem.id}</div>
+            <div>Created: {elem.created}</div>
+            <div>Name: {elem.name}</div>
+            <div>Status: {elem.status}</div>
+            <div>Type: {elem.type}</div>
+            <div>Gender: {elem.gender}</div>
+            <div>Species: {elem.species}</div>
           </div>
         </div>
       )}
-    </PagesLayout>
+    </div>
   );
 };
