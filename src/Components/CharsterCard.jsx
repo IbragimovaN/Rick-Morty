478596d21a8622dc2a5df80e89c8fafaf52c6a6d
Anastasiya <anastasiya.ibr@mail.ru>
2024-12-20 22:26:48 +0300

    styles: стили сменились на адаптив

--- src/Components/CharsterCard.jsx
+++ src/Components/CharsterCard.jsx
@@ -1,49 +1,24 @@
-// import { Link } from "react-router-dom";
-// import { internalPaths } from "../internalPath";
-// import { forwardRef, memo } from "react";
-
-// const CharsterCard = forwardRef(({ ...character }, ref) => {
-//   const { name, species, image, created } = character;
-
-//   return (
-//     <div
-//       className="max-w-xs rounded  shadow-lg m-4 bg-white hover:shadow-xl transition-shadow duration-300 "
-//       ref={ref}
-//     >
-//       <Link to={internalPaths.character(character.id)}>
-//         <img className="w-full h-100 object-cover" src={image} alt={name} />
-
-//         <div className="p-4">
-//           <h3 className="text-xl font-bold text-gray-800">{name}</h3>
-//           <div className="text-gray-600 mt-1">{created}</div>
-//           <div className="text-gray-600 mt-1">{species}</div>
-//         </div>
-//       </Link>
-//     </div>
-//   );
-// });
-// export default memo(CharsterCard);
-
 import { Link } from "react-router-dom";
-import { internalPaths } from "../internalPath";
+import { internalPaths } from "../../../../constants/internalPath";
 import { forwardRef, memo } from "react";
 import { Card } from "antd";
-
-const { Meta } = Card;
+import { formatDate } from "../../../../utils/formatDate";
 
 const CharsterCard = forwardRef(({ ...character }, ref) => {
   const { name, species, image, created } = character;
+  const formatedCreated = formatDate(created);
 
   return (
-    <div ref={ref} className="m-4">
+    <div ref={ref}>
       <Link to={internalPaths.character(character.id)}>
         <Card
           hoverable
           style={{ width: 300 }}
           cover={<img src={image} alt={name} />}
         >
-          <Meta title={name} description={species} />
-          <div className="text-gray-600 mt-1">{created}</div>
+          <div>{name}</div>
+          <div>{species}</div>
+          <div>{formatedCreated}</div>
         </Card>
       </Link>
     </div>
