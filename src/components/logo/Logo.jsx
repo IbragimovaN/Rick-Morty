5232bf5028b34eb605166bc8e72a187bb70592dd
Anastasiya <anastasiya.ibr@mail.ru>
2024-12-21 18:55:31 +0300

    feat: изменена архитектура

---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { internalPaths } from "../../constants/internalPath";
+import logoImage from "../../assets/rick-1.png";
+import { Image } from "antd";
+
+export const Logo = () => {
+  return (
+    <Link to={internalPaths.home}>
+      <Image
+        src={logoImage}
+        preview={false}
+        style={{
+          maxWidth: "350px",
+          width: "100%",
+          height: "auto",
+          display: window.innerWidth < 768 ? "none" : "block",
+        }}
+      />
+    </Link>
+  );
+};
---
+++ src/pages/charactersPage/CharactersPage.jsx
@@ -0,0 +1,8 @@
+import CharsterCard from "./components/characterCard/CharsterCard";
+import { CommonPage } from "../../components";
+
+const CharactersPage = () => {
+  return <CommonPage CardComponent={CharsterCard} />;
+};
+
+export default CharactersPage;
